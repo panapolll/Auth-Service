eb2776cfff35b24316475594b8d4a361a79fedaf
Giss <panaponsukcharoen@email.com>
2026-06-29 14:34:34 +0700

    add CORS support

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,11 @@ dns.setServers(['8.8.8.8', '1.1.1.1']);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*', // dev ใช้ * ได้ก่อน
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
   await app.listen(process.env.PORT ?? 3100);
   console.log('ไม่มี error นะไอน้อง');
 }
